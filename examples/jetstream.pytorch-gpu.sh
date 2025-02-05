7305170d68e9e9e000895aa1d430a0fb76a7a475
Victoria Nguyen <104641464+vin011@users.noreply.github.com>
2025-02-05 09:57:45 -0800

    Update jetstream.pytorch-gpu.sh

--- examples/jetstream.pytorch-gpu.sh
+++ examples/jetstream.pytorch-gpu.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-export SINGULARITY_MODULE=apptainer
-export PYTHON_MODULE=python/3.8.12/7zdjza7
+export SINGULARITY_MODULE=singularity/1.3.4
 export GITHUB_REPO=$HOME/NVIDIA-Singularity-Containers
 export ENV_OUPUT_FILE_NAME=$SLURM_JOB_ID.environment.txt
 
 module reset
-module load $SINGUALRITY_MODULE $PYTHON_MODULE
-
-cd /scratch
+module load $SINGUALRITY_MODULE 
 
 ## Print relevant environment and resource variables
 module list >> $ENV_OUTPUT_FILE_NAME
@@ -15,7 +12,5 @@ printenv >> $ENV_OUTPUT_FILE_NAME
 nvidia-smi >> $ENV_OUTPUT_FILE_NAME
 
 # Load and run container
-tar -xzvf $GITHUB_REPO/pytorch_24.11-py3.sif.gz
-
-singularity exec --bind /scratch pytorch_24.11-py3.sif "python3 -c 'import torch; print(torch.cuda.is_available()')"
+apptainer exec pytorch_24.11-py3.sif python -c 'import torch; print(torch.cuda.is_available())'
 
