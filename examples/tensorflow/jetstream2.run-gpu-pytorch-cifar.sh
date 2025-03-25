8a1bf76f58a489cbc6f7b0ba88e09c8e1d0d8574
Victoria Nguyen <vin011@login01.expanse.sdsc.edu>
2025-03-25 16:52:34 -0700

    Updated jetstream tensorflow script

---
+++ examples/tensorflow/jetstream2.run-gpu-pytorch-cifar.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+export SINGULARITY_MODULE=singularity/1.1.2
+export CONTAINER_DIR=$HOME/tensorflow_23.10-py3.sif
+export GITHUB_REPO_DIR=$HOME/NVIDIA-Singularity-Containers
+
+module reset
+module load $SINGULARITY_MODULE
+
+## Print relevant environment and resource variables
+module list 
+printenv 
+nvidia-smi 
+
+time -p singularity exec --nv $CONTAINER_DIR python3 $GITHUB_REPO_DIR/examples/tensorflow/tensorflow.cifar.py
