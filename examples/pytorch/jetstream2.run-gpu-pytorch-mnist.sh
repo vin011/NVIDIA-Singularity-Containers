1df1c3fd747b8dc99c2cb371ab874bd4bed95605
Victoria Nguyen <vin011@login01.expanse.sdsc.edu>
2025-03-25 17:43:04 -0700

    Updated jetstream pytorch script

--- examples/pytorch/jetstream2.run-gpu-pytorch-mnist.sh
+++ examples/pytorch/jetstream2.run-gpu-pytorch-mnist.sh
@@ -1,29 +1,14 @@
 #!/usr/bin/env bash
-### Please adjust the following variables accordingly
-#SBATCH --job-name=pytorch-gpu
-#SBATCH --account=sds196
-#SBATCH --partition=gpu-shared
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=90G
-#SBATCH --gpus=1
-#SBATCH --time=00:30:00
-#SBATCH --output=pytorch-gpu.o%j.%N
-
-export SINGULARITY_MODULE=singularitypro/4.1.2
-export CONTAINER_DIR=$HOME/pytorch_23.02-py3.sif
+export SINGULARITY_MODULE=singularity/1.1.2
+export CONTAINER_DIR=$HOME/pytorch_23.10-py3.sif
 export GITHUB_REPO_DIR=$HOME/NVIDIA-Singularity-Containers
 
 module reset
 module load $SINGULARITY_MODULE
 
-#### We will run from the scratch directory local to the node
-cd /scratch/$USER/job_$SLURM_JOBID
-
 ## Print relevant environment and resource variables
-module list 
-printenv 
-nvidia-smi 
+module list
+printenv
+nvidia-smi
 
-time -p singularity exec --bind /expanse,/scratch --nv $CONTAINER_DIR python3 $GITHUB_REPO_DIR/examples/pytorch/pytorch.mnist.py
+time -p singularity exec --nv $CONTAINER_DIR python3 $GITHUB_REPO_DIR/examples/pytorch/pytorch.mnist.py
